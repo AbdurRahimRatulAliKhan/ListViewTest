fb38268e69e0003c91d9cb055e7eef2f74942a87
Ratul Ali <rahim498617@gmail.com>
2018-09-16 05:25:23 +0600

    Initial commit

---
+++ app/src/main/java/com/example/dell/listviewtest/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.dell.listviewtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    /*String[] dist = {"Dhaka", "Chattagram", "Khulna", "Barisal", "Noakhali", "Comilla", "Chadpur", "Feni", "Gazipur", "Rajshahi"};*/
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.lvId);
+       final String[] dist = getResources().getStringArray(R.array.dist);
+
+        ArrayAdapter adapter = new ArrayAdapter(this,R.layout.sample_layout, R.id.tvItemId, dist);
+
+        listView.setAdapter(adapter);
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selected = dist [position];
+                Toast.makeText(MainActivity.this, "" + selected, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
